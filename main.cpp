536293fa032f32e775fa993526812567b5a11d10
Nahom Melese <nahomjr17@gmail.com>
2025-01-17 11:26:00 -0800

    adding library

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <limits>
+#include <algorithm>
 
 using namespace std;
 
+
+struct City {
+    int id;
+    double x, y;
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Hello wwWorld!" << endl;
     return 0;
 }
