4b2cc9498c5dff06e1b08c6f9047e59fd3af7f76
Nahom Melese <nahomjr17@gmail.com>
2025-01-17 17:56:14 -0800

    calculate the shortest distance for small subsets

--- main.cpp
+++ main.cpp
@@ -14,6 +14,15 @@ double calculateDistance(const Point& a, const Point& b) {
     return sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2));
 }
 
+double bruteForce(vector<Point>& points, int left, int right) {
+    double minDist = numeric_limits<double>::max();
+    for (int i = left; i <= right; ++i) {
+        for (int j = i + 1; j <= right; ++j) {
+            minDist = min(minDist, calculateDistance(points[i], points[j]));
+        }
+    }
+    return minDist;
+}
 int main()
 {
 
